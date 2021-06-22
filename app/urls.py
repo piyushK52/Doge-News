d07d4434696076e1b853ffd031bbec59139ae73f
piyushK52 <piyush.kumar.iaf@gmail.com>
2021-06-22 23:51:50 +0530

    main routes added

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('account.v1.urls')),
+    url(r'^post/', include('post.v1.urls')),
+    url(r'^auth/', include('authentication.v1.urls')),
 ]
